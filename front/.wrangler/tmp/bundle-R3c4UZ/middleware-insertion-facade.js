f2a3060ffbf6b546e7c65fc8d49729977ff9e1e6
IIDUTTII <justindutt4215@gmail.com>
2026-06-23 20:01:22 +0530

    all order status done with user sync3

---
+++ front/.wrangler/tmp/bundle-R3c4UZ/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "/home/dutt/Desktop/mywebsite/front/.wrangler/tmp/pages-CIKRGZ/functionsWorker-0.09677495624671684.mjs";
+				import * as __MIDDLEWARE_0__ from "/usr/local/lib/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "/usr/local/lib/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
+
+				export * from "/home/dutt/Desktop/mywebsite/front/.wrangler/tmp/pages-CIKRGZ/functionsWorker-0.09677495624671684.mjs";
+				const MIDDLEWARE_TEST_INJECT = "__INJECT_FOR_TESTING_WRANGLER_MIDDLEWARE__";
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
